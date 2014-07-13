f1132d4927c0afad4235c38a0b762e36a5699752
Maxim Kouprianov <maxim@kouprianov.com>
2014-07-13 16:54:08 +0400

    Progess.

--- api.go
+++ api.go
@@ -3,6 +3,7 @@ package smsru
 import (
 	"bufio"
 	"errors"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strconv"
@@ -54,7 +55,28 @@ type Sms struct {
 	Multiple  map[string]string
 }
 
-func (a *Api) SmsSend(sms *Sms) (ids []string, err error) {
+func Token() (token string, err error) {
+	var cli http.Client
+	var resp *http.Response
+	u, _ := url.Parse(baseURL)
+	u.Path = authGetToken
+	if resp, err = cli.Get(u.String()); err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	var tok []byte
+	if tok, err = ioutil.ReadAll(resp.Body); err != nil {
+		return
+	}
+	if len(tok) < 1 {
+		err = ErrBadServerReply
+		return
+	}
+	token = string(tok)
+	return
+}
+
+func (a *Api) smsUrl(sms *Sms) (u *url.URL) {
 	val := make(url.Values)
 	if len(sms.Multiple) > 0 {
 		for to, text := range sms.Multiple {
@@ -80,9 +102,15 @@ func (a *Api) SmsSend(sms *Sms) (ids []string, err error) {
 		val.Add("partner_id", sms.PartnerId)
 	}
 
-	u, _ := url.Parse(baseURL)
-	u.Path = smsSend
+	u, _ = url.Parse(baseURL)
 	u.RawQuery = val.Encode()
+	return
+}
+
+func (a *Api) SmsSend(sms *Sms) (ids []string, err error) {
+	u := a.smsUrl(sms)
+	u.Path = smsSend
+
 	var lines []string
 	if lines, err = a.get(u); err != nil {
 		return
@@ -116,6 +144,100 @@ func (a *Api) SmsSend(sms *Sms) (ids []string, err error) {
 	return
 }
 
+func (a *Api) SmsStatus(id string) (err error) {
+	val := make(url.Values)
+	val.Add("id", id)
+	u, _ := url.Parse(baseURL)
+	u.Path = smsSend
+	u.RawQuery = val.Encode()
+	_, err = a.get(u)
+	return
+}
+
+func (a *Api) SmsCost(sms *Sms) (cost float32, n int, err error) {
+	u := a.smsUrl(sms)
+	u.Path = smsCost
+	var lines []string
+	if lines, err = a.get(u); err != nil {
+		return
+	}
+	if len(lines) < 2 {
+		err = ErrBadServerReply
+		return
+	}
+	var c float64
+	if c, err = strconv.ParseFloat(lines[0], 32); err != nil {
+		return
+	}
+	cost = float32(c)
+	var num uint64
+	if num, err = strconv.ParseUint(lines[1], 10, 8); err != nil {
+		return
+	}
+	n = int(num)
+	return
+}
+
+func (a *Api) MyBalance() (balance float32, err error) {
+	u, _ := url.Parse(baseURL)
+	u.Path = myBalance
+	var lines []string
+	if lines, err = a.get(u); err != nil {
+		return
+	}
+	if len(lines) < 1 {
+		err = ErrBadServerReply
+		return
+	}
+	var bal float64
+	if bal, err = strconv.ParseFloat(lines[0], 32); err != nil {
+		return
+	}
+	balance = float32(bal)
+	a.Balance = balance
+	return
+}
+
+func (a *Api) MyLimit() (limit int, n int, err error) {
+	u, _ := url.Parse(baseURL)
+	u.Path = myLimit
+	var lines []string
+	if lines, err = a.get(u); err != nil {
+		return
+	}
+	if len(lines) < 2 {
+		err = ErrBadServerReply
+		return
+	}
+	var lim, num uint64
+	if lim, err = strconv.ParseUint(lines[0], 10, 0); err != nil {
+		return
+	}
+	limit = int(lim)
+	if num, err = strconv.ParseUint(lines[0], 10, 0); err != nil {
+		return
+	}
+	n = int(num)
+	return
+}
+
+func (a *Api) MySenders() (senders []string, err error) {
+	u, _ := url.Parse(baseURL)
+	u.Path = mySenders
+	var lines []string
+	if lines, err = a.get(u); err != nil {
+		return
+	}
+	if len(lines) < 1 {
+		err = ErrBadServerReply
+		return
+	}
+	for _, l := range lines {
+		senders = append(senders, l)
+	}
+	return
+}
+
 func (a *Api) auth(param url.Values) url.Values {
 	param.Add("api_id", a.Token)
 	return param
@@ -135,12 +257,18 @@ func (a *Api) get(u *url.URL) (lines []string, err error) {
 	sc := bufio.NewScanner(resp.Body)
 	if sc.Scan() {
 		var i int64
-		i, err = strconv.ParseInt(sc.Text(), 10, 8)
+		i, err = strconv.ParseInt(sc.Text(), 10, 16)
 		if err != nil {
 			return
 		}
-		if err = Error(int(i)); err != nil {
-			return
+		if u.Path == smsStatus {
+			if err = Status(int(i)); err != nil {
+				return
+			}
+		} else {
+			if err = Error(int(i)); err != nil {
+				return
+			}
 		}
 	} else {
 		return nil, ErrBadServerReply
--- api_test.go
+++ api_test.go
@@ -8,16 +8,17 @@ import (
 )
 
 const testToken = "ddc5b4d1-4563-2344-658c-63ba15f677a2"
+const devPhone = "79269965690" // your dev phone, the cost should be 0.
 
 var simpleSms = &Sms{
-	To:   "79269965690",
+	To:   devPhone,
 	Text: "Привет, Xlab!",
 	Test: true,
 }
 
 var multiSms = &Sms{
 	Multiple: map[string]string{
-		"79269965690": "Земля",
+		devPhone:      "Земля",
 		"79853329310": "Воздух",
 	},
 	Test: true,
@@ -37,4 +38,45 @@ func TestSmsSend(t *testing.T) {
 	}
 	log.Printf("Sent multiple sms: %s", ids)
 	log.Printf("Balance: %f", api.Balance)
+}
+
+func TestSmsCost(t *testing.T) {
+	api := NewApi(testToken)
+	cost, n, err := api.SmsCost(simpleSms)
+	assert.NoError(t, err)
+	assert.Condition(t, func() bool { return cost == 0 }, "cost of messages should be 0")
+	assert.Condition(t, func() bool { return n == 1 }, "n of messages should be 1")
+
+	// API bug on the serverside
+	// cost, n, err = api.SmsCost(multiSms)
+	// assert.NoError(t, err)
+	// assert.Condition(t, func() bool { return cost > 1 }, "cost of messages should be > 0")
+	// assert.Condition(t, func() bool { return n == 2 }, "n of messages should be 2")
+}
+
+func TestMyBalance(t *testing.T) {
+	api := NewApi(testToken)
+	balance, err := api.MyBalance()
+	assert.NoError(t, err)
+	assert.Condition(t, func() bool { return balance > 0 }, "balance should be > 0")
+}
+
+func TestMyLimit(t *testing.T) {
+	api := NewApi(testToken)
+	limit, _, err := api.MyLimit()
+	assert.NoError(t, err)
+	assert.Condition(t, func() bool { return limit > 0 }, "limit should be > 0")
+}
+
+func TestMySenders(t *testing.T) {
+	api := NewApi(testToken)
+	senders, err := api.MySenders()
+	assert.NoError(t, err)
+	assert.NotEmpty(t, senders)
+}
+
+func TestToken(t *testing.T) {
+	token, err := Token()
+	assert.NoError(t, err)
+	assert.Condition(t, func() bool { return len(token)/2 == 16 }, "size of token should be 16 bit")
 }
